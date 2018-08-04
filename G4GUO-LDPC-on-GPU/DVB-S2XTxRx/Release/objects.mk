b8f016a97e87ea0bacc6050b25cd911efd01ec7a
Abraxas3d <abraxas3d@optimizedtomfoolery.com>
2018-08-04 14:25:22 -0700

    updated codebase

--- G4GUO-LDPC-on-GPU/DVB-S2XTxRx/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lLimeSuite -liio -lcusolver -lcublas -lcusparse
-
